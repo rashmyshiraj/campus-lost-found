646c05aac95f993220e32cfcd2dbd05c37c386c5
Rashmy Shiraj <rashmyshiraj1@gmail.com>
2026-03-08 17:49:38 +0530

    Initial project setup MERN

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('CampusLost&Found API is running');
+});
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error(err));
